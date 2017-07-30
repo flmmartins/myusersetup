5506dfeab9a879def667628b664898e6703a881a
Fernanda Martins <flmmartins@gmail.com>
2017-07-30 13:31:39 +0200

    Configure keyboard, pkg manager and sanity checks

--- configure.sh
+++ configure.sh
@@ -1,14 +1,90 @@
 #!/bin/bash
-echo "Installing Vim and Git"
-apt-get install vim git curl wget
+OS=''
+PACKAGE_MANAGER=''
+VUNDLE_DIR="~/.vim/bundle/Vundle.vim"
 
+discover_os_and_pkg_manager () {
+  OS_CMD=$(uname)
+  case "$OS_CMD" in
+    'Linux')
+      OS=$(awk '{print $1}' /etc/issue)
+      if [[ "$OS" == "Ubuntu" ]]; then
+        PACKAGE_MANAGER="apt"
+      else
+        PACKAGE_MANAGER="yum"
+      fi
+      ;;
+    'WindowsNT')
+      OS='Windows'
+      #TODO: Create container
+      PACKAGE_MANAGER="yum"
+      ;;
+    'Darwin')
+      OS='MAC'
+      PACKAGE_MANAGER="brew"
+      #TODO: Install brew
+      ;;
+    *)
+      echo "---------------------------------"
+      echo "Error"
+      echo "---------------------------------"
+      exit 1
+      ;;
+  esac
+  echo "OS is $OS"
+}
+
+switch_caps_to_ctrl () {
+  echo "---------------------------------"
+  echo "Keyboard Configuration"
+  echo "---------------------------------"
+  case "$OS" in
+    'Ubuntu')
+      sudo mv /etc/default/keyboard /etc/default/keyboard.old
+      sudo cp ./ubuntu_keyboard /etc/default/keyboard
+      sudo /usr/bin/setxkbmap -option "caps:ctrl_modifier"
+      ;;
+    'MAC')
+      ;;
+    'Windows')
+      echo "---------------------------------"
+      echo "Not necessary to configure keyboard on $OS"
+      echo "---------------------------------"
+      ;;
+    *)
+      #TODO: Test in CentOS
+      sudo xmodmap -e 'keycode 66 = Control_L'
+      sudo xmodmap -e 'clear Lock'
+      sudo xmodmap -e 'add Control = Control_L'
+      sudo xmodmap -pke >~/.Xmodmap
+      sudo xmodmap .Xmodmap
+      ;;
+  esac
+}
+
+discover_os_and_pkg_manager
+switch_caps_to_ctrl
+
+echo "---------------------------------"
+echo "Installing Vim and Git"
+echo "---------------------------------"
+sudo $PACKAGE_MANAGER install vim git curl wget
+
+echo "---------------------------------"
 echo "Configuring Git and Vim"
+echo "---------------------------------"
 cp .gitconfig ~/.gitconfig
 cp .vimrc ~/.vimrc
 
-echo "Installing Vundle"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+if [ ! -d "$VUNDLE_DIR" ]; then
+  echo "---------------------------------"
+  echo "Installing Vundle"
+  echo "---------------------------------"
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  vim +PluginInstall +qall
+fi
 
+echo "---------------------------------"
 echo "Installing Oh My ZSH"
-sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "---------------------------------"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
